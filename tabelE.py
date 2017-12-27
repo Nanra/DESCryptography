97e7bb8b51d633654b9c1d4f08561cfa8de1585e
Nanra Sukedy <sukedynanra@gmail.com>
2017-12-27 23:22:03 +0700

    Update for Tabel E

--- tabelE.py
+++ tabelE.py
@@ -1,6 +1,6 @@
 m = []
 s = 1
-IP = []
+E = []
 print "Proses Permutasi Dengan Tabel E"
 print "Masukkan Biner : "
 for i in range(1, 33):
@@ -15,7 +15,7 @@ for i in range(1, 33):
 #     m.append(s)
 #     s += 1
 
-IP = [m[32-1], m[1-1], m[2-1], m[3-1], m[4-1], m[5-1],
+E = [m[32-1], m[1-1], m[2-1], m[3-1], m[4-1], m[5-1],
       m[4-1], m[5-1], m[6-1], m[7-1], m[8-1], m[9-1],
       m[8-1], m[9-1], m[10-1], m[11-1], m[12-1], m[13-1],
       m[12-1], m[13-1], m[14-1], m[15-1], m[16-1], m[17-1],
@@ -27,14 +27,14 @@ IP = [m[32-1], m[1-1], m[2-1], m[3-1], m[4-1], m[5-1],
 print ("%"*80)
 print(m)
 print ("%"*80)
-print IP
+print E
 print ("%"*80)
-print "Hasil Setelah melalui Tabel IP\n"
-print IP[0:6], IP[6:12], IP[12:18], IP[18:24]
+print "Hasil Setelah melalui Tabel E\n"
+print E[0:6], E[6:12], E[12:18], E[18:24]
 print "\n"
-print IP[24:30], IP[30:36], IP[36:42], IP[42:48]
+print E[24:30], E[30:36], E[36:42], E[42:48]
 print "\n"
 print ("%"*80)
-print "E(Rn-1) = ",IP[0:48]
+print "E(Rn-1) = ", E[0:48]
 print ("%"*80)
-print "Jumlah Bit",len(IP)
+print "Jumlah Bit",len(E)
